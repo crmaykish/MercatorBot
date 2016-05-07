0d2f9c8f1164cfe6cb3adb9dd69ff7e3483982b0
Colin Maykish <crmaykish@gmail.com>
2016-05-07 10:05:43 -0400

    Started work on NRF24L01 radio

--- mercatorbot-avr/mercatorbot main/main.c
+++ mercatorbot-avr/mercatorbot main/main.c
@@ -2,29 +2,67 @@
 
 #include "bitmacros.h"
 #include "serial.h"
+#include "analog.h"
+#include "nrf24l01.h"
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
 
-#define LED PD6
-#define SWITCH PD7
-#define POT PA7
+#define CE	PB3;
+#define CSN	PB4;
 
-int num = 0;
+char SPIWriteByte(unsigned char b) {
+	SPDR = b;
+	while (!(SPSR & bitValue(SPIF)));
+	return SPDR;
+}
+
+uint8_t getRegister(uint8_t reg) {
+	_delay_us(10);
+	clearBit(PORTB, CSN);
+	_delay_us(10);
+	SPIWriteByte(R_REGISTER + reg);
+	_delay_us(10);
+	reg = SPIWriteByte(NOP);
+	_delay_us(10);
+	setBit(PORTB, CSN);
+	return reg;
+}
+
+void writeToNRF(uint8_t reg, uint8_t package) {
+	_delay_us(10);
+	clearBit(PORTB, CSN);
+	_delay_us(10);
+	SPIWriteByte(W_REGISTER + reg);
+	_delay_us(10);
+	SPIWriteByte(package);
+	_delay_us(10);
+	setBit(PORTB, CSN);
+}
 
 void setup() {
-	// Set up digital IO
-	setBit(DDRD, LED);
-	clearBit(DDRD, SWITCH);
-
+	analogInit();
 	serialInit();
 	stdout = &uartOut;
+
+	// SCK pin
+	DDRB |= bitValue(PB7);
+
+	// MOSI pin
+	DDRB |= bitValue(PB5);
+
+	// NRF CE pin
+	DDRB |= bitValue(CE);
+
+	// NRF CSN pin
+	DDRB |= bitValue(CSN);
+
+	setBit(PORTB, CSN);
+	clearBit(PORTB, CE);
 }
 
 void loop() {
-	printf("hello %d \n", num);
-	num++;
-	_delay_ms(300);
+	
 }
 
 int main(void) {
