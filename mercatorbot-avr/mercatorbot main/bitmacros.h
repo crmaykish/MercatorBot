8d536fa7d2c6ffacde503a6e5bd7ce72c022e33b
Colin Maykish <crmaykish@gmail.com>
2016-05-08 21:59:38 -0400

    Prep for ultrasonic sensor code

--- mercatorbot-avr/mercatorbot main/bitmacros.h
+++ mercatorbot-avr/mercatorbot main/bitmacros.h
@@ -1,9 +1,9 @@
-#ifndef COLIN_H_
-#define COLIN_H_
+#ifndef BITMACROS_H_
+#define BITMACROS_H_
 
 #define bitValue(x)			(1 << x)
 #define setBit(P, B)		P |= bitValue(B)
 #define clearBit(P, B)		P &= ~bitValue(B)
 #define toggleBit(P, B)		P ^= bitValue(B)
 
-#endif /* COLIN_H_ */
+#endif /* BITMACROS_H_ */
