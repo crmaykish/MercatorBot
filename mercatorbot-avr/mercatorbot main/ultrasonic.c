f73c22904396a27d0c22cdbe9c2c10d0fa2051fb
Colin Maykish <crmaykish@gmail.com>
2016-05-12 22:58:36 -0400

    Quick fix for ultrasonic sensor trigger interrupt. Ready to implement timers and calculations

--- mercatorbot-avr/mercatorbot main/ultrasonic.c
+++ mercatorbot-avr/mercatorbot main/ultrasonic.c
@@ -6,26 +6,17 @@
 #include <stdio.h>
 
 void ultrasonicInit() {
-	// Set triggers as output, leave echos as input
+	// Set trigger pins as output, leave echo pins as input
 	DDRC |= bitValue(TRIG1);
 	DDRC |= bitValue(TRIG2);
 	DDRC |= bitValue(TRIG3);
-
-	clearBit(PORTC, TRIG1);
-	clearBit(PORTC, TRIG2);
-	clearBit(PORTC, TRIG3);
-
-	// Pull-up resistors?
-	//PORTC |= bitValue(ECHO1);
-	//PORTC |= bitValue(ECHO2);
-	//PORTC |= bitValue(ECHO3);
 
 	// Mask for PCIE2 interrupt
 	PCMSK2 |= bitValue(ECHO1);
 	PCMSK2 |= bitValue(ECHO2);
 	PCMSK2 |= bitValue(ECHO3);
 
-	// Enable interrupt PCIE2 (PCINT23:16)
+	// Enable interrupt PCIE2 (PORTC, PCINT23:16)
 	PCICR |= bitValue(PCIE2);
 
 	// Enable global interrupts
@@ -33,7 +24,6 @@ void ultrasonicInit() {
 }
 
 ISR(PCINT2_vect) {
-	printf("interrupt!");
-	printf((PINC & bitValue(PC2)) == 1 ? "high" : "low");
-	printf("\n");
+	int i = (PINC & bitValue(ECHO3)) >> ECHO3;
+	printf("%s\n", i ? "high" : "low");
 }
